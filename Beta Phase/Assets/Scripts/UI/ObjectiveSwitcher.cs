cc87ff321da3927c0fcd3c622e4fd7b116d602ab
dygold <44193932+dygold@users.noreply.github.com>
2019-01-16 23:08:22 +0800

    Basic Tutorial Dialogue Skeleton
    Can't do much for now, will explain if needed.

---
+++ Beta Phase/Assets/Scripts/UI/ObjectiveSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveSwitcher : MonoBehaviour {
+
+    [SerializeField]
+    Text objectiveText;
+
+    public void ChangeObjective(string newObjective)
+    {
+        objectiveText.text = newObjective;
+    }
+}
