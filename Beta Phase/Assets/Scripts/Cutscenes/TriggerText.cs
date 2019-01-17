bb7c51c896bd407fc4a61089182df5ea105f6152
dygold <44193932+dygold@users.noreply.github.com>
2019-01-17 12:59:13 +0800

    Tutorial Text popup script

---
+++ Beta Phase/Assets/Scripts/Cutscenes/TriggerText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TriggerText : MonoBehaviour
+{
+
+    public Text tutorialText;
+    [SerializeField]
+    string instructions;
+    bool happened;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !happened)
+        {
+            TutText();
+        }
+    }
+
+    public void TutText()
+    {
+        StopCoroutine("TutTextBehaviour");
+        StartCoroutine("TutTextBehaviour", instructions);
+        happened = true;
+    }
+
+    IEnumerator TutTextBehaviour(string instructions)
+    {
+        tutorialText.text = instructions;
+        yield return new WaitForSeconds(8);
+        tutorialText.text = "";
+    }
+
+}
