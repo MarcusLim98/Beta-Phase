53c3801a044012c97af73d06013740b9d432c40c
Hyder <harits.satryo@yahoo.com>
2019-01-30 17:38:26 +0800

    Updated Main Menu BGM and Bottle pick up sfx, Added Vent bgm,
    Will adjust the bottle pick up volume. Now's too soft

--- Beta Phase/Assets/Scripts/AmbienceControl.cs
+++ Beta Phase/Assets/Scripts/AmbienceControl.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class AmbienceControl : MonoBehaviour
 {
     public AudioSource Ambient;
-    string water, vent;
+    string  vent;
+    public float dripTimer = 10f;
+    public bool dripStart;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Ambient = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Ambient.isPlaying)
+        {
+            vent = "Vent";
+            Ambient.clip = (AudioClip)Resources.Load(vent);
+
+            Ambient.Play();
+        }
     }
 }
