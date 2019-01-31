1d707e4b4d3b0c8ab2c84c97a475f3ea8880b5da
Hyder <harits.satryo@yahoo.com>
2019-01-31 09:54:42 +0800

    Ambience Control
    For NIght scenes specifically. Looping drip and owl sounds as well.

--- Beta Phase/Assets/Scripts/AmbienceControl.cs
+++ Beta Phase/Assets/Scripts/AmbienceControl.cs
@@ -4,25 +4,62 @@ using UnityEngine;
 
 public class AmbienceControl : MonoBehaviour
 {
-    public AudioSource Ambient;
-    string  vent;
-    public float dripTimer = 10f;
-    public bool dripStart;
+    public AudioSource Ambient1,Ambient2,Ambient3,Ambient4;
+    string  vent , bgm , drip , owl;
+    public float dripTimer, owlTimer;
+    public bool dripStart , owlStart;
+
     // Start is called before the first frame update
     void Start()
     {
-        Ambient = GetComponent<AudioSource>();
+        dripStart = false;
+        owlStart = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!Ambient.isPlaying)
+        //for Ambient and Vent
+        if (!Ambient1.isPlaying || !Ambient2.isPlaying)
         {
             vent = "Vent";
-            Ambient.clip = (AudioClip)Resources.Load(vent);
+            bgm = "night ambience";
+            Ambient1.clip = (AudioClip)Resources.Load(vent);
+            Ambient2.clip = (AudioClip)Resources.Load(bgm);
+            Ambient1.PlayOneShot(Ambient1.clip , 0.2f);
+            Ambient2.PlayOneShot(Ambient2.clip , 0.5f);
+        }
+        //for Drip loop
+        if(dripTimer >= 0)
+        {
+            dripTimer -= Time.deltaTime;
+        }
+        if(!Ambient3.isPlaying &&dripTimer <= 0)
+        {
+            drip = "Water drip";
+            Ambient3.clip = (AudioClip)Resources.Load(drip);
+            Ambient3.Play();
+            
+        }
+        if(dripTimer <= 0)
+        {
+            dripTimer = 12f;
+        }
+        //for Owl loop
+        if (owlTimer >= 0)
+        {
+            owlTimer -= Time.deltaTime;
+        }
+        if (!Ambient4.isPlaying && owlTimer <= 0)
+        {
+            owl = "Owl";
+            Ambient4.clip = (AudioClip)Resources.Load(owl);
+            Ambient4.Play();
 
-            Ambient.Play();
+        }
+        if (owlTimer <= 0)
+        {
+            owlTimer = 16f;
         }
     }
 }
