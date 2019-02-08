5447bf8adc6b0cd8b2a847ce4e79e0b1da347cf5
dygold <44193932+dygold@users.noreply.github.com>
2019-02-09 00:07:40 +0800

    Ending Scene
    Encountered game-breaking bugs which have thus been fixed.

--- Beta Phase/Assets/Scripts/Cutscenes/EndingCtrl.cs
+++ Beta Phase/Assets/Scripts/Cutscenes/EndingCtrl.cs
@@ -1,20 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndingCtrl : MonoBehaviour
 {
+    public CameraLogic cameraL;
+    public Transform yingYue;
+    public Vector3 targetOffset;
+    public float panSpeed;
+    public RPGTalk rpgT;
 
-
-
-    
     void Start()
     {
-        
+        StartCoroutine(PanOut());
     }
 
-    void Update()
+    IEnumerator PanOut()
     {
-        
+        yield return new WaitForSeconds(4);
+        cameraL.movementSpeed = panSpeed;
+        cameraL.targetOffset = targetOffset;
+        cameraL.player = yingYue;
+        yield return new WaitForSeconds(2);
+        rpgT.NewTalk();                         //otherwise, text speed causes bugs
     }
 }
