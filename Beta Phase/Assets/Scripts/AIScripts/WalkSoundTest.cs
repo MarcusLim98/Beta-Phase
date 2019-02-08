f3ebc5b29e7d9a85c677d835dbda5538ad93647b
Hyder <harits.satryo@yahoo.com>
2019-02-08 16:46:11 +0800

    Thug Footstep sync to Anim.
    Created script to work together with Animation Event functions

---
+++ Beta Phase/Assets/Scripts/AIScripts/WalkSoundTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkSoundTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource footSource;
+    string right, left;
+    void Start()
+    {
+        right = "ThugWalkR";
+        left = "ThugWalkL";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void RightFoot()
+    {
+        footSource.clip = (AudioClip)Resources.Load(right);
+        footSource.Play();
+    }
+    void LeftFoot()
+    {
+        footSource.clip = (AudioClip)Resources.Load(left);
+        footSource.Play();
+    }
+}
