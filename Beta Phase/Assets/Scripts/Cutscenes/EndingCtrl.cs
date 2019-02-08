7f191837c28a73b98159bc1a74e09cb532116231
dygold <44193932+dygold@users.noreply.github.com>
2019-02-08 17:02:19 +0800

    uwu

---
+++ Beta Phase/Assets/Scripts/Cutscenes/EndingCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingCtrl : MonoBehaviour
+{
+
+
+
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
