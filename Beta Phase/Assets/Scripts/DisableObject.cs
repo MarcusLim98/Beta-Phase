99d5c8bfbdf81634d6ad5338022cca74c249572c
Marcus Lim <marcus.haloODST@gmail.com>
2019-02-18 02:54:56 +0800

    Scene 8 tweaking

--- Beta Phase/Assets/Scripts/DisableObject.cs
+++ Beta Phase/Assets/Scripts/DisableObject.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class DisableObject : MonoBehaviour {
 
     public GameObject[] toDisappear;
+    public Collider[] colliders;
+    public bool forColliders;
     // Use this for initialization
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.name == "Player")
         {
-            foreach(GameObject obj in toDisappear)
+            if (!forColliders)
             {
-                obj.SetActive(false);
-                this.gameObject.SetActive(false);
+                foreach (GameObject obj in toDisappear)
+                {
+                    obj.SetActive(false);
+                    this.gameObject.SetActive(false);
+                }
+            }
+
+            if (forColliders)
+            {
+                foreach (Collider obj in colliders)
+                {
+                    obj.enabled = false;
+                }
             }
         }
     }
